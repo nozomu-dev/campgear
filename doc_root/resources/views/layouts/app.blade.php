07bc3fe90724a197a67f818a3d4baaceb98b7067
松下望 <matsu.uner@gmail.com>
2018-06-17 12:29:33 +0900

    ナビゲーション「記事一覧」をログイン後のみ表示に変更

--- doc_root/resources/views/layouts/app.blade.php
+++ doc_root/resources/views/layouts/app.blade.php
@@ -34,9 +34,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a href="/articles">記事一覧</a>
-                        </li>
+
                     </ul>
 
                     <!-- Right Side Of Navbar -->
@@ -50,6 +48,9 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="/articles">記事一覧</a>
+                            </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
