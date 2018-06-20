c1625bc787da44d8a6b55a90889214777dbf036b
松下望 <matsu.uner@gmail.com>
2018-06-20 13:16:44 +0900

    migrationファイルを作成

---
+++ doc_root/database/migrations/2018_06_19_155044_create_gears_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGearsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gears', function (Blueprint $table) {
+            $table->increments('gear_id');
+            $table->text('gear_name');
+            $table->integer('gear_category_id');
+            $table->integer('owning_user_id');
+            $table->integer('repository_id');
+            $table->text('gear_description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gears');
+    }
+}
---
+++ doc_root/database/migrations/2018_06_20_041038_create_repositories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepositoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('repositories', function (Blueprint $table) {
+            $table->increments('repository_id');
+            $table->text('repository_name');
+            $table->text('repository_address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('repositories');
+    }
+}
