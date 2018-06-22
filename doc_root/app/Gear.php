1a3bcd4b0bdcceceb53be79cca75e67c556f506d
松下望 <matsu.uner@gmail.com>
2018-06-23 01:53:44 +0900

    プライマリーキーを変更

--- doc_root/app/Gear.php
+++ doc_root/app/Gear.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class gear extends Model
 {
-    //
+    protected $primaryKey = 'gear_id';
 }
---
+++ doc_root/app/GearCategory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GearCategory extends Model
+{
+    protected $primaryKey = 'gear_category_id';
+}
