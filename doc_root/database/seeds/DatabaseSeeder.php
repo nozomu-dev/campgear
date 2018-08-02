776cdeb1349cbedc157c5cf7b854f224b40e1593
松下望 <matsu.uner@gmail.com>
2018-08-03 00:15:24 +0900

    作成・編集したseederを登録

--- doc_root/database/seeds/DatabaseSeeder.php
+++ doc_root/database/seeds/DatabaseSeeder.php
@@ -12,5 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call([
+            UsersTableSeeder::class,
+            GearsTableSeeder::class,
+            GearCategoriesTableSeeder::class,
+            RepositoriesTableSeeder::class,
+            UserGroupsTableSeeder::class
+        ]);
     }
 }
